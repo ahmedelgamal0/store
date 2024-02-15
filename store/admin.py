47e305728d4f7b55d07640c4dfde6d0341982be5
ahmedelgamal0 <a.elgamal98@outlook.com>
2024-02-15 15:14:51 +0200

    customized admin

--- store/admin.py
+++ store/admin.py
@@ -1,60 +1,107 @@
-from django.db.models import Count
-from typing import Any
+from ast import mod
+from cgitb import lookup
+from turtle import update
 from django.contrib import admin
+from django.db.models import Count
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
+from typing import Any
+
 from . import models
 
 
 admin.site.site_header = 'Camel Store'
 admin.site.index_title = 'Store Administration'
 
-
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
-    search_fields = ['title']
+    search_fields = ['title__istartswith']
     list_per_page = 10
+    
     
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
     
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return super().get_queryset(request).annotate(
             products_count=Count('product')
         )
-    
 
+    
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
-    search_fields = ['title']
+    list_filter = ['collection']
+    search_fields = ['title__istartswith']
     list_per_page = 10
-    # list_select_related = ['collection']
-    
-    # def collection_title(self, product):
-    #     return product.collection.title
     
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         return 'Low' if product.inventory < 10 else 'OK'
     
+    @admin.action(description='clear_inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.',
+        )
+    
     
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email', 'phone', 'membership']
+    list_display = ['first_name', 'last_name', 'email', 'phone', 'membership', 'orders']
     list_editable = ['membership']
-    search_fields = ['email']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     list_per_page = 10
     
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+              'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+        
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    model = models.OrderItem
+    extra = 0
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'payment_status', 'customer', 'customer_email']
-    list_per_page = 10
     list_select_related = ['customer']
+    list_filter = ['customer']
+    inlines = [OrderItemInline]
+    list_per_page = 10
 
-    def customer_email(self, order):
+
+    def customer_email(self, order): 
         return order.customer.email
--- storefront/settings.py
+++ storefront/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'store',
     'tags',
     'likes',
-    'playground'
+    'playground',
+    'store_custom'
 ]
 
 MIDDLEWARE = [
