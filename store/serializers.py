a927986a3b2b86ab0ab2e1d91a1d2093bcb67e81
ahmedelgamal0 <a.elgamal98@outlook.com>
2024-06-10 15:18:42 +0300

    collection APIs

---
+++ store/serializers.py
@@ -0,0 +1,42 @@
+from dataclasses import fields
+from decimal import Decimal
+from gc import collect
+from pyexpat import model
+from rest_framework import serializers
+
+from .models import Product, Collection
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "description",
+            "slug",
+            "inventory",
+            "unit_price",
+            "price_with_tax",
+            "collection",
+        ]
+
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.4)
+
+    # #OVERWRITE
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title", "products_count"]
+
+    products_count = serializers.IntegerField()
+
